6be810be7d9685e58ba302bdb46319de9b0c06be
DebNayak05 <143987174+DebNayak05@users.noreply.github.com>
2025-02-16 04:48:47 +0530

    updated Todos.jsx

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -19,38 +19,3 @@ function Todos({ todos, onTodoUpdated }) {
   );
 }
 export default Todos;
-      {/* todos is an array which contains all todos. the main issue is now rendering that arrat */}
-      {/* {todos.map( (todoItem) => {
-            return <div key={todoItem._id} className="p-5">
-                <h1>{todoItem.title}</h1>
-                <h1>{todoItem.description}</h1>
-                <button onClick={ () => {
-                    fetch("http://localhost:3000/markDone", {
-                        method:"PUT",
-                        body: JSON.stringify({
-                            id:todoItem._id
-                        }),
-                        headers: {
-                            "Content-Type":"application/json"
-                        }
-                    })
-                    
-                }}>
-                    { todoItem.completed ? "Completed!!" : "Mark as done"} 
-                </button>
-                <button onClick={ () => {
-                    fetch("http://localhost:3000/deleteTodo", {
-                        method:"DELETE",
-                        body: JSON.stringify({
-                            id:todoItem._id
-                        }),
-                        headers: {
-                            "Content-Type":"application/json"
-                        }
-                    })
-                    
-                }}>
-                    Delete
-                </button>
-            </div>
-        }) } */}
